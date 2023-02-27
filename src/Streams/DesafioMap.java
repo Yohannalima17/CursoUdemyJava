27cf2efebb5c230aad61e6e56e9e10112431365a
Yohanna L.(Not) <yohanna.lima16@hotmail.com>
2023-02-27 10:42:53 -0300

    27/02/2023

---
+++ src/Streams/DesafioMap.java
@@ -0,0 +1,14 @@
+package Streams;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DesafioMap {
+
+    public static void main(String[] args) {
+
+        List<Integer> nums = Arrays.asList(1,2,3,4,5,6,7,8,9);
+
+        nums.stream().map(Integer::toBinaryString).forEach(System.out::println);
+    }
+}
